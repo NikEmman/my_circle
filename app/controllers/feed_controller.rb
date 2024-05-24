cd36955ca8d7574075880751993cd5ca20c9880b
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-05-25 01:52:33 +0300

    Modify feed to show user and followees' posts

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,5 +1,7 @@
 class FeedController < ApplicationController
   def index
-    @posts = current_user.posts.order(created_at: :desc)
+    @posts = current_user.posts
+    @posts += current_user.followees.flat_map(&:posts)
+    @posts.sort_by! { |post| -post.created_at.to_i }
   end
 end
