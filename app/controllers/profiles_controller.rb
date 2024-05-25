2c8d03326faac0bfa5d598ab99052bdacb5a6025
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-05-25 20:25:03 +0300

    Add profile controller and some views

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,46 @@
+class ProfilesController < ApplicationController
+  before_action :require_permission, only: %i[edit destroy]
+  before_action :set_profile, only: %i[show edit update destroy]
+  def new
+    @profile = Profile.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def create
+    @profile = current_user.profile.build(profile_params)
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to profile_path, notice: 'Profile was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @profile.update(profile_params)
+        format.html { redirect_to profile_path, notice: 'Profile was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name)
+  end
+
+  def require_permission
+    return unless current_user != Profile.find(params[:id]).user
+
+    redirect_to root_path, alert: 'You are not the owner of this profile'
+  end
+end
