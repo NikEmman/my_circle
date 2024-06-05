8d89d4daad12383ebdeefd81bcbeb67b41c83915
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-06-05 19:43:22 +0300

    Add more post controller tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   let(:user) { create(:user) }
-  let(:post) { create(:post, user:) }
 
   describe 'prevent access to unauthed users' do
-    it 'should not render index page' do
+    let(:user) { create(:user) }
+
+    let(:post) { create(:post) }
+
+    it 'should not be successful' do
       get posts_path
-      expect(response).not_to render_template :index
+      expect(response).not_to be_successful
     end
   end
   context 'with user signed in ' do
+    let(:user) { create(:user) }
+    let(:my_post) { create(:post, user:) }
     before { sign_in user }
+    before { create(:profile, user:) }
+
     describe 'GET /index' do
       it 'should render index page' do
         get posts_path
@@ -26,15 +33,41 @@ RSpec.describe 'Posts', type: :request do
     end
     describe 'Get /Edit' do
       it 'should render edit page' do
-        get edit_post_path(post)
+        get edit_post_path(my_post)
         expect(response).to render_template :edit
       end
     end
     describe 'Post /Post' do
       it 'should create a post with valid attributes' do
-        post '/posts', params: { post: attributes_for(:post) }
-        expect(response).to redirect_to posts_path
+        post posts_path, params: { post: FactoryBot.attributes_for(:post) }
+        expect(response).to redirect_to root_path
         expect(flash[:notice]).to eq 'Post was successfully created.'
+      end
+      it 'should not create a post with invalid attributes' do
+        post '/posts', params: { post: FactoryBot.attributes_for(:post, body: nil) }
+        expect(response).to render_template :new
+        expect(flash[:notice]).to eq nil
+      end
+    end
+    describe 'Put /Post' do
+      it 'should update a post with valid attributes' do
+        put "/posts/#{my_post.id}", params: { post: FactoryBot.attributes_for(:post) }
+        expect(response).to redirect_to post_path(my_post)
+        expect(flash[:notice]).to eq 'Post was successfully updated.'
+      end
+
+      it 'should not update a post with invalid attributes' do
+        put "/posts/#{my_post.id}", params: { post: FactoryBot.attributes_for(:post, body: nil) }
+        expect(response).to render_template :edit
+        expect(flash[:notice]).to eq nil
+      end
+    end
+
+    describe 'Delete /post' do
+      it 'should destroy a post' do
+        delete "/posts/#{my_post.id}"
+        expect(response).to redirect_to posts_url
+        expect(flash[:notice]).to eq 'Post was successfully destroyed.'
       end
     end
   end
