79a0656c7e727ff160e7438d458309cebc0ef8ca
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-06-16 04:59:24 +0300

    Update profile form, add user name normalization

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,8 +3,16 @@ class Profile < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   has_one_attached :avatar
+  before_save :normalize_name
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def normalize_name
+    self.first_name = first_name.squish.titleize
+    self.last_name = last_name.squish.titleize
   end
 end
